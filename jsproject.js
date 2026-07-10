ad0c006f92d1f80b8151217d29dacea32be6e1e5
Zain Ul Abideen <zimirajpoot273@gmail.com>
2026-07-10 18:26:08 +0500

    booleamm test

--- jsproject.js
+++ jsproject.js
@@ -1,8 +1,15 @@
-const firstName = "Zain UL Abideen";
-const lastName = "Rajpoot";
+// const firstName = "Zain UL Abideen";
+// const lastName = "Rajpoot";
 
-const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
-const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+// const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+// const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
 
-console.log(sentence);
-console.log(sentenceWithTemplate);
+// console.log(sentence);
+// console.log(sentenceWithTemplate);
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+  console.log("The sky is blue!");
+} else {
+  console.log("The sky is … not blue?");
+}
