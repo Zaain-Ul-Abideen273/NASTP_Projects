225d81be40bbf47cc64abe2cfcc5e18bb51c3209
Zain Ul Abideen <zimirajpoot273@gmail.com>
2026-07-10 20:15:23 +0500

    Example 2 Camprison

--- jsproject.js
+++ jsproject.js
@@ -17,11 +17,23 @@
 //   console.log("The sky is … not blue?");
 // }
 
-// if you see three lines, it's just three = in a row, ===. the font just combines them into one big character
-if (2 + 2 === 4) {
-  console.log(
-    "Oh thank god, the fundamental principles of mathematics still hold true."
-  );
+
+// // if you see three lines, it's just three = in a row, ===. the font just combines them into one big character
+// if (2 + 2 === 4) {
+//   console.log(
+//     "Oh thank god, the fundamental principles of mathematics still hold true."
+//   );
+// } else {
+//   console.log("Uh, panic?");
+// }
+
+
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 0) {
+  console.log("Cool, now I have a lot of nachos to myself.");
+} else if (friendsAtYourParty <= 4) {
+  console.log("Perfect amount to play some Mario Kart.");
 } else {
-  console.log("Uh, panic?");
+  console.log("Wooooo turn on the dance music!");
 }
